f38ef8f053bf31bc85d2df8c5f491079dcdd535e
Kredam <akitlosz2000@gmail.com>
2022-02-07 20:11:08 +0100

    Extended stats json

--- pages/api/githubAPI.js
+++ pages/api/githubAPI.js
@@ -6,5 +6,16 @@ export default async(req, res) => {
     })
     const {data: followers} = await octokit.request("/users/Kredam/followers?per_page=100");
     const {data: my_repos} = await octokit.rest.repos.listForAuthenticatedUser();
-    return res.status(200).json({repos: my_repos.length, followers: followers.length})
+    const stargazersCount = my_repos.filter(repo => !repo.fork).reduce((curr, next) => {return curr + next.stargazers_count}, 0);
+    const watchersCount = my_repos.filter(repo => !repo.fork).reduce((curr, next) => {return curr + next.watchers_count}, 0);
+    const reposCount = my_repos.length;
+    let publicReposCount = my_repos.filter(repo => !repo.fork && !repo.private).length;
+    const numberOfCollabs = my_repos.filter(repo => repo.owner.login !== "Kredam").length;
+    return res.status(200).json({   reposCount: reposCount, 
+                                    followersCount: followers.length,
+                                    stargazersCount: stargazersCount,
+                                    watchersCount: watchersCount,
+                                    publicReposCount: publicReposCount,
+                                    privateReposCount: reposCount - publicReposCount,
+                                    numberOfCollabs: numberOfCollabs})
 }
