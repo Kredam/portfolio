6e8a39b6e4957144985fd3486427c065549bd768
Kredam <akitlosz2000@gmail.com>
2022-02-07 20:54:11 +0100

    missed commits before merge(stat mapping)

--- pages/api/githubAPI.js
+++ pages/api/githubAPI.js
@@ -4,6 +4,8 @@ export default async(req, res) => {
     const octokit = new Octokit({
         auth: process.env.GITHUB_AUTH_TOKEN
     })
+    const statNames= ['Repos', 'Followers', 'Stargazers', 'Watchers', 'Public Repos', 'Private Repos', 'Collabs'];
+
     const {data: followers} = await octokit.request("/users/Kredam/followers?per_page=100");
     const {data: my_repos} = await octokit.rest.repos.listForAuthenticatedUser();
     const stargazersCount = my_repos.filter(repo => !repo.fork).reduce((curr, next) => {return curr + next.stargazers_count}, 0);
@@ -11,11 +13,11 @@ export default async(req, res) => {
     const reposCount = my_repos.length;
     let publicReposCount = my_repos.filter(repo => !repo.fork && !repo.private).length;
     const numberOfCollabs = my_repos.filter(repo => repo.owner.login !== "Kredam").length;
-    return res.status(200).json({   reposCount: reposCount, 
-                                    followersCount: followers.length,
-                                    stargazersCount: stargazersCount,
-                                    watchersCount: watchersCount,
-                                    publicReposCount: publicReposCount,
-                                    privateReposCount: reposCount - publicReposCount,
-                                    numberOfCollabs: numberOfCollabs})
+    return res.status(200).json({   Repos: reposCount, 
+                                    Followers: followers.length,
+                                    Stargazers: stargazersCount,
+                                    Watchers: watchersCount,
+                                    PublicRepos: publicReposCount,
+                                    PrivateRepos: reposCount - publicReposCount,
+                                    Collabs: numberOfCollabs})
 }
--- src/components/projects/statistics.js
+++ src/components/projects/statistics.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Typography, Grid, CircularProgress, Card, CardContent } from "@mui/material";
 import useSWR from "swr";
 
@@ -8,28 +10,41 @@ const fetcher = async(...args) => await fetch(...args).then(response => response
 const Statistics = () => {
 
     const { data, error } = useSWR('http://localhost:3000/api/githubAPI', fetcher);
-
+    const statNames= ['Repos', 'Followers', 'Stargazers', 'Watchers', 'Public Repos', 'Private Repos', 'Collabs'];
     return(
-        <Grid sx={{marginTop:'40px'}} container direction='column'>
+        <Grid sx={{marginTop:'40px'}} container justifyContent="center" alignItems='center'>
             <Grid item sx={{marginBottom:'25px'}}>
-                <Typography variant='h4' align="center">Statistics</Typography>
+                <Typography variant='h4' align="center">GitHub <FontAwesomeIcon icon={faGithub}/> Statistics</Typography>
             </Grid>
-            <Grid item container>
+            <Grid item container justifyContent='center' alignItems='center'>
                 {data ? Object.entries(data).map( ([key, value]) => {
-                    console.log(key, value);
                     return <>
-                        <Grid item xs>
-                            <Card>
+                        <Grid item xs sx={{margin:2}}>
+                            <Card sx={{width:150}}>
                                 <CardContent>
-                                    <Typography>{key}</Typography>
+                                    <Typography align="center">{key}</Typography>
                                 </CardContent>
                                 <CardContent>
-                                    <Typography key={value}>{value}</Typography>
+                                    <Typography aling="center">{value}</Typography>
                                 </CardContent>
                             </Card>
                         </Grid>
                     </>
-                }) : 'Not working'}
+                }) : statNames.map((stat) => {
+                        return <>
+                            <Grid item xs>
+                                <Card sx={{width:150}}>
+                                    <CardContent>
+                                        <Typography align="center">{stat}</Typography>
+                                    </CardContent>
+                                    <CardContent>
+                                        <Typography align="center"><CircularProgress /></Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </>
+                })
+                }
             </Grid>
         </Grid>
     )
